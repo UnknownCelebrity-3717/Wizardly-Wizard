1dc2dcf41b375b825e8c56dce7932d8494464264
UnknownCelebrity-3717 <aakarshsingh813@gmail.com>
2024-07-26 22:39:29 +0530

    Added a Java program to subtract two numbers

---
+++ lala.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Difference
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,diff=0;
+        System.out.println("Enter a Number: ");
+        a=sc.nextInt();
+        System.out.println("Enter another number: ");
+        b=sc.nextInt();
+        diff=a-b;
+        System.out.println("Sum: "+sum);
+    }
+}

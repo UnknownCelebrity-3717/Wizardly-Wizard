1a14b9b9e177ef95936248020debcc1c1ce585a0
UnknownCelebrity-3717 <aakarshsingh813@gmail.com>
2024-07-28 19:28:02 +0530

    Update lala.java

--- lala.java
+++ lala.java
@@ -4,13 +4,13 @@ class Difference
     public static void main()
     {
         Scanner sc = new Scanner(System.in);
-        int a,b,diff=0;
+        int a,b,diff;
         System.out.println("Enter a Number: ");
         a=sc.nextInt();
         System.out.println("Enter another number: ");
         b=sc.nextInt();
         diff=a-b;
-        System.out.println("Sum: "+sum);
-        System.out.println("Hello World!");
+        System.out.println("Diff: "+diff);
+        System.out.println("Hey! There.");
     }
 }
